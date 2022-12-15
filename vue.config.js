7826ea1023e82fabbe8f8c3308e98d9a506f1d64
875778261 <875772861@qq.com>
2022-12-16 01:02:22 +0800

    bbb

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,15 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   lintOnSave: false,
   transpileDependencies: true,
+  outputDir:"dist",
+  publicPath:"./",
   devServer: {
     open: true,
     host: 'localhost',
     proxy: {
       '/api': {
-        target: 'http://1.116.64.64:5004/api2/',//目标地址
+        //target: 'http://1.116.64.64:5004/api2/',//目标地址
+        target: process.env.VUE_APP_API,
         changeOrigin: true,//允许跨域
         pathRewrite: {
           '^/api': ''
